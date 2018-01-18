e2e2205ef713e0b9857188f5f2aaa8e8dcf9007c
Melissa He <melissahe@ac.c4q.nyc>
2018-01-18 10:22:38 -0500

    Initial Commit

---
+++ CoreLocation-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreLocation-Test
+//
+//  Created by C4Q on 1/18/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
